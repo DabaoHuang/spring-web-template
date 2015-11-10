338b68112613c97fb44e4db32b493a38a5f55897
Ocean <ocean@caizhenhuadeMacBook-Pro.local>
2015-11-10 16:30:02 +0800

    測試機器的controller.

---
+++ src/main/java/ocean/control/TestControl.java
@@ -0,0 +1,26 @@
+package ocean.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by ocean on 2015/11/10.
+ * 用來測試 web 是否有無回應
+ */
+
+@Controller
+@RequestMapping("/test")   //spring mapping url
+public class TestControl {
+    /**詢問 api 是否有在正常運作*/
+    @ResponseBody
+    @RequestMapping(value="/ready", method = RequestMethod.GET)
+    public Object login(HttpSession session){
+        return "YES";
+    }
+
+}
