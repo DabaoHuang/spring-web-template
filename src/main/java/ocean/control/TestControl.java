a14ae5e8bce865a054ad017adbe3408256693201
Ocean <ocean@caizhenhuadeMacBook-Pro.local>
2015-11-10 16:31:15 +0800

    清除空白與多餘的import.

--- src/main/java/ocean/control/TestControl.java
+++ src/main/java/ocean/control/TestControl.java
@@ -3,7 +3,6 @@ package ocean.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
@@ -22,5 +21,4 @@ public class TestControl {
     public Object login(HttpSession session){
         return "YES";
     }
-
 }
