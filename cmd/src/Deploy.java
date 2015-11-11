be09c31d0f4205ce717a88808aae6187fd201099
Ocean <ocean@caizhenhuadeMacBook-Pro.local>
2015-11-11 15:49:42 +0800

    工具 script.

---
+++ cmd/src/Deploy.java
@@ -0,0 +1,10 @@
+package src;
+
+/**
+ * Created by ocean on 2015/11/11.
+ * 預計動作
+ * 1. 靜態資源部署於靜態空間。 (css js images ..)
+ * 2. 動態資源部署於 web service。
+ */
+public class Deploy {
+}
---
+++ cmd/src/Switch.java
@@ -0,0 +1,10 @@
+package src;
+
+/**
+ * Created by ocean on 2015/11/11.
+ *
+ * 切換 production 、 test 、 develop，環境設定。
+ *
+ */
+public class Switch {
+}
